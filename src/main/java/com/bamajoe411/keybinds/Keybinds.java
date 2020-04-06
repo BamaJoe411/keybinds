0e65b93f97fe2b09c6b69631c32cb0bb6f109e49
Jeremy Beane <bamajoe411@jb-debian-zbook>
2020-04-05 20:05:39 -0400

    added secod keybind and lang support

--- src/main/java/com/bamajoe411/keybinds/Keybinds.java
+++ src/main/java/com/bamajoe411/keybinds/Keybinds.java
@@ -16,22 +16,36 @@ public class Keybinds implements ModInitializer{
     @Override
     public void onInitialize() {
         KeyBindingRegistry.INSTANCE.addCategory("Vanilla High");
-        keyBinding = FabricKeyBinding.Builder.create(
-                new Identifier("sleepers","key"),
+        // Home
+        FabricKeyBinding home = FabricKeyBinding.Builder.create(
+                new Identifier("keybinds", "home"),
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_EQUAL,
                 "Vanilla High"
         ).build();
-        KeyBindingRegistry.INSTANCE.register(keyBinding);
+        KeyBindingRegistry.INSTANCE.register(home);
+
+        // Spawn
+        FabricKeyBinding spawn = FabricKeyBinding.Builder.create(
+                new Identifier("keybinds", "spawn"),
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_MINUS,
+                "Vanilla High"
+        ).build();
+        KeyBindingRegistry.INSTANCE.register(spawn);
 
         ClientTickCallback.EVENT.register(e ->
         {
-            if(keyBinding.isPressed()) {
+            if(home.isPressed()) {
                 World world = MinecraftClient.getInstance().world;
 
                 //ClientPlayerEntity player = new ClientPlayerEntity(MinecraftClient.getInstance(), world);
 
-                System.out.println("the key was pressed!");
+                System.out.println("Home Key Was Pressed");
+            }
+
+            if(spawn.isPressed()) {
+                System.out.println("Spawn Key Was Pressed");
             }
         });
 
